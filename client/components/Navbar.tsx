d79af6b2f324386c66b5ec2c70faf4c228f0f206
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-08 09:24:13 +0800

    render pow token balances

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { useAddress, useContractRead, useContract } from "@thirdweb-dev/react";
 import Link from 'next/link'
+
+import { abi, contractAddress } from '../constants/Token/token'
+
 
 interface NavbarProps {
   linkHref: string;
@@ -8,12 +12,20 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ linkHref, linkText }) => {
+  const address = useAddress()
+  const { contract } = useContract(contractAddress, abi);
+
+  const { data, isLoading, error } = useContractRead(contract, "getBalanceOfAddress", [address]);
+
   return (
     <nav className="flex items-center justify-between bg-black p-5">
         <Link href={'/dashboard'}>
           <span className="text-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent animate-text font-black">Proof of Workout Protocol</span>
         </Link>
         <div className="flex items-center space-x-2">
+            <h1 className='text-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent animate-text font-bold mr-10'>
+              💪 {Number(data)/1000000000000000000} POW tokens
+            </h1>
             <Link href={linkHref}>
                 <button className='bg-white hover:bg-gradient-to-r hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 hover:animate-text px-5 py-2 rounded-lg'>
                     {linkText}
---
+++ client/constants/Token/token.ts
@@ -0,0 +1,5 @@
+import abi_data from '../../../contracts/out/ProofOfWorkoutToken.sol/ProofOfWorkoutToken.json'; 
+
+export const abi = abi_data.abi;
+export const contractAddress = '0x92DFBD87ec23120223593E903A5e753772E4039e';
+
