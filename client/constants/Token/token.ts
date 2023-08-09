6fa7df93f71df0d5aea46ff3c97214ab259e9ab1
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-09 22:35:45 +0800

    change token abi path

--- client/constants/Token/token.ts
+++ client/constants/Token/token.ts
@@ -1,4 +1,4 @@
-import abi_data from '../../../contracts/out/ProofOfWorkoutToken.sol/ProofOfWorkoutToken.json'; 
+import abi_data from './ProofOfWorkoutToken.json'; 
 
 export const abi = abi_data.abi;
 export const contractAddress = '0x92DFBD87ec23120223593E903A5e753772E4039e';
