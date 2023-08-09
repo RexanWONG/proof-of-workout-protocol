c1fd61dd069b235dd421614eaee39447d4d7a3c5
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-09 22:31:46 +0800

    change abi path

--- client/constants/QuestManager/questManager.ts
+++ client/constants/QuestManager/questManager.ts
@@ -1,4 +1,4 @@
-import abi_data from '../../../contracts/out/QuestManager.sol/QuestManager.json'; 
+import abi_data from './QuestManager.json'; 
 
 export const abi = abi_data.abi;
 export const contractAddress = '0xD2FD5B0Ed1d8c79E03a40ED1caD6e9fcDc39d3F4';
