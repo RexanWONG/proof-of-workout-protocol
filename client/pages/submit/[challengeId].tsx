c2a24c9e20cad4a708adcb4f8c1cce489babbbca
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-10 14:43:44 +0800

    change redirect url

--- client/pages/submit/[challengeId].tsx
+++ client/pages/submit/[challengeId].tsx
@@ -27,7 +27,7 @@ const Submit = () => {
     const { challengeId } = router.query; 
 
     const STRAVA_CLIENT_ID = process.env.NEXT_STRAVA_CLIENT_ID
-    const STRAVA_OAUTH_LINK = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3001/submit/${challengeId}&approval_prompt=force&scope=activity:read`
+    const STRAVA_OAUTH_LINK = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIENT_ID}&response_type=code&redirect_uri=https://proof-of-workout-protocol-cosensys.vercel.app/submit/${challengeId}&approval_prompt=force&scope=activity:read`
     
     const [isLoading, setIsLoading] = useState(false)
     const [activitiesList, setActivitiesList] = useState<ActivitiesResponse | null>(null);
